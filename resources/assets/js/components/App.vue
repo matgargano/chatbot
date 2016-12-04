6df24f32e5ddd0c670ecb25168765037b45c93b4
Mat Gargano <mgargano@gmail.com>
2016-12-04 16:45:04 -0500

    send chat, scroll to bottom

--- resources/assets/js/components/App.vue
+++ resources/assets/js/components/App.vue
@@ -20,6 +20,18 @@
     width:300px;
 }
 
+.chat-input {
+    padding:5px;
+    width:100%;
+}
+
+.chat-box {
+    max-height:250px;
+    overflow-y:auto;
+}
+
+
+
 
 
 
@@ -42,15 +54,19 @@
                         <small v-else>offline</small>
                     </div>
 
-
-                    <div class="messages" v-for="message in messages">
-                        <div class="panel-body">
-                            <div class="clearfix">
-                                <blockquote class="pull-right" :class="[{admin: message.person_id != personId}, {user:  message.person_id == personId}]">{{ message.message }}</blockquote>
+                    <div class="chat-box" id="chat-box">
+                        <div class="message" v-for="message in messages">
+                            <div class="panel-body">
+                                <div class="clearfix">
+                                    <blockquote class="pull-right"
+                                                :class="[{admin: message.person_id != personId}, {user:  message.person_id == personId}]">
+                                        {{ message.message }}
+                                    </blockquote>
+                                </div>
                             </div>
                         </div>
                     </div>
-                    <input type="text" v-model="currentChatMessage" @keyup.enter="submitChat">
+                    <input class="chat-input" type="text" v-model="currentChatMessage" @keyup.enter="submitChat">
                 </div>
             </div>
 
@@ -84,6 +100,11 @@
             }
         },
         methods: {
+            scrollBottom(){
+                var objDiv = document.getElementById("chat-box");
+                objDiv.scrollTop = objDiv.scrollHeight;
+
+            },
             initChat(){
                 this.$http.post('http://chatservice.dev/api/person', {
                     name: this.name,
@@ -110,9 +131,14 @@
 
             },
             getChat(){
+                var $this = this;
+
                 this.$http.get('http://chatservice.dev/api/chatMessage', {params:  {id: this.chatId}})
                     .then(response =>{
                             this.messages = response.data.data;
+                            setTimeout(function(){
+                                this.scrollBottom();
+                            }.bind(this),1); // push to the bottom of the execution stack
                         }, error => {
                             //@todo error handling?
                             console.log(error);
@@ -130,14 +156,16 @@
 
             },
             submitChat(){
+            var chatMessage = this.currentChatMessage;
+            this.currentChatMessage = '';
                 this.$http.post('http://chatservice.dev/api/chatMessage',
                             {
                                 chat_id: this.chatId,
                                 person_id: this.personId,
-                                message: this.currentChatMessage
+                                message: chatMessage
                             })
                             .then(response =>{
-
+                                  this.getChat();
                                 }, error => {
                                     //@todo error handling?
                                     console.log(error);
@@ -148,23 +176,8 @@
 
         }
 
+
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 
