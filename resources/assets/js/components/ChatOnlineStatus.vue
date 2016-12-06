21a986056eb2a06ea0caa22a11af2dc3d2da14b0
Mat Gargano <mgargano@gmail.com>
2016-12-06 08:06:40 -0500

    add orphan chatonlinestatus

---
+++ resources/assets/js/components/ChatOnlineStatus.vue
@@ -0,0 +1,36 @@
+<style scoped>
+    .offline {
+        background:red;
+    }
+</style>
+
+<template>
+    <div class="panel-heading" :class="{offline: !online}"> Support
+        <small v-if="online">online</small>
+        <small v-else>offline</small>
+    </div>
+
+</template>
+<script>
+
+    import {dataBus} from "../app";
+
+    export default {
+        data(){
+            return {
+
+                online: true, // set to false and heading becomes red
+
+            };
+
+        },
+
+        methods: {
+            online(){
+                // check if online using http request and a RESTful endpoint to be created
+            }
+        }
+
+    }
+
+</script>
